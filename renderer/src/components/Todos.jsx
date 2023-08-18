1cb846fb3334fd99a2f3c9c2aed5a87771106401
Jonatan041101 <valdiviezojonatan.2101@gmail.com>
2023-08-18 00:32:08 -0300

    feat: add logic and jsx in component Todos

--- renderer/src/components/Todos.jsx
+++ renderer/src/components/Todos.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useRef, useState } from 'react';
+import CreateTodo from '../molecules/CreateTodo';
 
 export default function Todos() {
+  const [todos, setTodos] = useState([]);
+  const refId = useRef(1);
+  const handleCreateNote = (todo) => {
+    const newTodo = {
+      id: refId,
+      text: todo,
+    };
+    refId.current = refId.current + 1;
+    setTodos([...todos, newTodo]);
+  };
   return (
     <div>
-      
+      <CreateTodo createTodo={handleCreateNote} />
+      {todos.map((todo) => (
+        <article key={todo.id}>{todo.text}</article>
+      ))}
     </div>
-  )
+  );
 }
