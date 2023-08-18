a1e0dd5a22f3ba48302125c7db8b50cb6b8cd918
Jonatan041101 <valdiviezojonatan.2101@gmail.com>
2023-08-18 01:47:08 -0300

    feat: use function the hook useTodo handleUpdateTodo

--- renderer/src/components/Todos.jsx
+++ renderer/src/components/Todos.jsx
@@ -3,12 +3,12 @@ import CreateTodo from '../molecules/CreateTodo';
 import Todo from './Todo';
 import useTodo from '../hooks/useTodo';
 export default function Todos() {
-  const { todos, handleCreateNote } = useTodo();
+  const { todos, handleCreateNote, handleUpdateTodo } = useTodo();
   return (
     <div>
       <CreateTodo createTodo={handleCreateNote} />
       {todos.map((todo) => (
-        <Todo todo={todo} key={todo.id} />
+        <Todo onUpdate={handleUpdateTodo} todo={todo} key={todo.id} />
       ))}
     </div>
   );
