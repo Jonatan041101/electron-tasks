8665b77d588eae067345063149fffea7440a1f47
Jonatan041101 <valdiviezojonatan.2101@gmail.com>
2023-08-18 00:43:36 -0300

    fix: repare variable of object newTodo to destructuring and add component Todo

--- renderer/src/components/Todos.jsx
+++ renderer/src/components/Todos.jsx
@@ -1,12 +1,13 @@
 import React, { useRef, useState } from 'react';
 import CreateTodo from '../molecules/CreateTodo';
 import { newTodoContract } from '../../utils/createTodo';
+import Todo from './Todo';
 
 export default function Todos() {
   const [todos, setTodos] = useState([]);
   const refId = useRef(1);
   const handleCreateNote = (todo) => {
-    const newTodo = newTodoContract(todo);
+    const { newTodo } = newTodoContract(todo, refId.current);
     refId.current = refId.current + 1;
     setTodos([...todos, newTodo]);
   };
@@ -14,7 +15,7 @@ export default function Todos() {
     <div>
       <CreateTodo createTodo={handleCreateNote} />
       {todos.map((todo) => (
-        <article key={todo.id}>{todo.text}</article>
+        <Todo todo={todo} key={todo.id} />
       ))}
     </div>
   );
