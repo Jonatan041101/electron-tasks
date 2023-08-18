cc58c2827d77509736a518a2c8555131254161d7
Jonatan041101 <valdiviezojonatan.2101@gmail.com>
2023-08-18 01:46:16 -0300

    feat: add logic update todo in component

--- renderer/src/components/Todo.jsx
+++ renderer/src/components/Todo.jsx
@@ -1,5 +1,18 @@
 import React from 'react';
+import ButtonEdit from '../atoms/ButtonEdit';
 
-export default function Todo({ todo }) {
-  return <article>{todo.text}</article>;
+export default function Todo({ todo, onUpdate }) {
+  const handleUpdateTodo = () => {
+    onUpdate(todo.id, todo.text);
+  };
+  return (
+    <article>
+      {todo.text}
+      <ButtonEdit
+        text="Editar"
+        handleClick={handleUpdateTodo}
+        isDisabled={false}
+      />
+    </article>
+  );
 }
