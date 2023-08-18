de9e5f83e1c68f46c8ddc798e397059f10c51268
Jonatan041101 <valdiviezojonatan.2101@gmail.com>
2023-08-18 02:22:33 -0300

    refactor: remove logic innecesary add hook useTodo

--- renderer/src/components/Todo.jsx
+++ renderer/src/components/Todo.jsx
@@ -1,28 +1,23 @@
-import React, { useEffect, useReducer } from 'react';
+import React, { useReducer } from 'react';
 import ButtonEdit from '../atoms/ButtonEdit';
 import Form from '../atoms/Form';
 import Input from '../atoms/Input';
 import ButtonComplete from '../atoms/ButtonComplete';
 import { isValidNote } from '../../utils/isValidNote';
-import { INITIAL_STATE, IS_EDIT, TEXT, reducer } from './reducer';
+import { INITIAL_STATE, reducer } from './reducer';
+import useTodo from '../hooks/useTodo';
 
 export default function Todo({ todo, onUpdate }) {
   const [state, dispatch] = useReducer(reducer, INITIAL_STATE);
-  useEffect(() => {
-    dispatch({ type: TEXT, payload: todo.text });
-  }, [todo.text, state.isEdit]);
-  const handleUpdateTodo = (evt) => {
-    evt.preventDefault();
-    onUpdate(todo.id, state.text);
-    handleChangeEditTodo();
-  };
-  const handleChange = (evt) => {
-    const { value } = evt.target;
-    dispatch({ type: TEXT, payload: value });
-  };
-  const handleChangeEditTodo = () => {
-    dispatch({ type: IS_EDIT });
-  };
+  const { handleChange, handleUpdateTodo, handleChangeEditTodo } = useTodo(
+    state.text,
+    state.isEdit,
+    todo.text,
+    todo.id,
+    onUpdate,
+    dispatch,
+  );
+
   const IS_DISABLED = isValidNote(state.text);
   const TEXT_BUTTON_EDIT = state.isEdit ? 'Cancelar' : 'Editar';
   return (
