5f5a307cf6b35fe28a4c477f782d5411674c7bea
Jonatan041101 <valdiviezojonatan.2101@gmail.com>
2023-08-18 00:24:28 -0300

    refactor: change component CreateTodo by Todos

--- renderer/pages/home.jsx
+++ renderer/pages/home.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import CreateTodo from '../src/molecules/CreateTodo';
+import Todos from '../src/components/Todos';
 
 function Home() {
   return (
     <main className="main">
       <h1 className="main__h1">Aplicación de notas</h1>
-      <CreateTodo />
+      <Todos />
     </main>
   );
 }
